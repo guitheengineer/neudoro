cf62c97cc187382db3c92fdcc60f2ce05a5c79e6
gsdeveloper <gsdevelopercontact@gmail.com>
2021-01-05 16:13:40 -0300

    fix: change timer to decrease by minute

--- src/components/timer-main/TimerMain.tsx
+++ src/components/timer-main/TimerMain.tsx
@@ -71,7 +71,7 @@ const TimerMain = () => {
     () => {
       dispatch(setTimer('decrease'));
     },
-    isActive && timer > 0 ? 1000 : null
+    isActive && timer > 0 ? 60000 : null
   );
 
   return (
